7dba81de91a7316c22820cbc3fd1e1e4ac6fbb9a
AlexZhao <zhaozixuan0729@126.com>
2020-02-19 15:06:27 -0600

    create submissions folder

--- tests.py
+++ tests.py
@@ -2,14 +2,16 @@ import argparse
 import csv
 
 # Evaluate points for a given pairing solution
-def calculate_points_for_pair(args, dataset_contents, pairing):
+def calculate_points_for_pair(args, dataset_contents, key, pairing):
     alpha = args.alpha
     beta = args.beta
     debug = args.debug
 
     student1 = dataset_contents[int(pairing[0]) + 1]
     student2 = dataset_contents[int(pairing[1]) + 1]
-    print(pairing)
+    if debug:
+        print("Evaluating pairing #{} pairing {}".format(key, pairing))
+    
     if student1[1] != student2[1]:
         if debug:
             print("Point 0: Student 1 and 2 not in same class")
@@ -32,7 +34,7 @@ if __name__ == "__main__":
     # Argument parsing
     parser = argparse.ArgumentParser()
     parser.add_argument('--dataset_path', type=str, default='./ilct_studentdata.csv', help='path to input data csv file')
-    parser.add_argument('--sol_path', type=str, default='./test.csv', help= 'path to the solution csv file')
+    parser.add_argument('--sol_path', type=str, default='./submissions/test_sol.csv', help= 'path to the solution csv file')
     parser.add_argument('--alpha', type=int, default='20', help='weight for preference')
     parser.add_argument('--beta', type=int, default='5', help='penalty for difference in classes')
     parser.add_argument('--lefterover_penalty', type=int, default='50', help='penalty for leaving someone alone')
@@ -41,6 +43,7 @@ if __name__ == "__main__":
     
     dataset_path = args.dataset_path
     sol_path = args.sol_path
+    debug = args.debug
 
     with open(dataset_path, "rt") as csvfile:
         dataset_contents = list(csv.reader(csvfile, delimiter = ','))
@@ -51,8 +54,11 @@ if __name__ == "__main__":
     
     points = 0
     for key, pairing in enumerate(sol_contents):
-        print("Evaluating pairing #", key)
-        points += calculate_points_for_pair(args, dataset_contents, pairing)
+        points += calculate_points_for_pair(args, dataset_contents, key, pairing)
     
-    print("Lonely student num: ", len(dataset_contents) - len(sol_contents))
+    num_leftover_students = len(dataset_contents) - len(sol_contents)
+    if debug: 
+        print("Lonely student num: ", num_leftover_students)
+    points -= num_leftover_students * args.lefterover_penalty
+
     print("Total point: {} \nTotal pairing: {}".format(points, len(sol_contents)))
